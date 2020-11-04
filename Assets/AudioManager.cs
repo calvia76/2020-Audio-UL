85063d37ab69eed450e7604346a312ed82517ae0
calvia76 <c.alvia@yahoo.com>
2020-11-03 20:47:47 -0500

    Taking Control of your Music step

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager manager;
+
+    public void Awake()
+    {
+        manager = this;
+    }
+
+
+    public AudioSource mainMusic;
+    public AudioSource auxMusic;
+    public AudioSource ambSound;
+
+    public AudioSource eventMusic;
+
+    public AudioMixerSnapshot eventSnap;
+    public AudioMixerSnapshot idleSnapshot;
+
+    public bool eventRunning;
+    public bool auxIn;
+
+    public AudioMixerSnapshot currentAudioMixerSnapshot;
+    public IEnumerator PlayEventMusic()
+    {
+        eventRunning = true;
+        eventSnap.TransitionTo(0.25f);
+        currentAudioMixerSnapshot = eventSnap;
+        yield return new WaitForSeconds(0.3f);
+        eventMusic.Play();
+        while (eventMusic.isPlaying)
+        {
+            yield return null;
+        }
+        eventRunning = false;
+        idleSnapshot.TransitionTo(.5f);
+        yield break;
+    }
+    
+
+}
